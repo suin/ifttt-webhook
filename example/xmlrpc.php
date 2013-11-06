3c498e1bb58e29901ecf34a19853fed0fea3dc15
suin <suinyeze@gmail.com>
2013-11-06 17:32:16 +0900

    update example

--- example/xmlrpc.php
+++ example/xmlrpc.php
@@ -1,19 +1,27 @@
 <?php
 
 use Suin\IftttWebhook\FakeWordPress;
-use Suin\IftttWebhook\Listeners\SlackIncomingWebHook;
+use Suin\IftttWebhook\Listener;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $xmlFromIFTTT = file_get_contents('php://input');
 
+class ClassListener implements Listener
+{
+    public function notify($username, $password, $title, $body, array $categories, array $tags)
+    {
+        // ... do something ...
+    }
+}
+
 $listeners = array(
     // Class listener
-    new SlackIncomingWebHook('oDX6yUMKRsXLpgqmarTu', '#general'),
+    new ClassListener(),
 
     // Closure listener
     function ($username, $password, $title, $body, $categories, $tags) {
-        // ...code...
+        // ...do something...
     },
 );
 $fakeWordPress = new FakeWordPress($listeners);
